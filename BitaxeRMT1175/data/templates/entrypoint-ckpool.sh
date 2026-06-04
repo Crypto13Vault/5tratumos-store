db5f5eed7ea6b2f34b3619a718f6a2c5ace5a5f4
Crypto13Vault <102202653+Crypto13Vault@users.noreply.github.com>
2026-06-04 08:04:21 +0100

    Delete BitaxeRMT1175 directory

--- BitaxeRMT1175/data/templates/entrypoint-ckpool.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-BTC_ADDR="$(grep -m1 '"btcaddress"' /config/ckpool.conf | sed 's/.*"btcaddress"[[:space:]]*:[[:space:]]*"\([^"]*\)".*/\1/')"
-
-echo "[BitaxeRMT1175] Starting ckpool for address: ${BTC_ADDR}"
-echo "[BitaxeRMT1175] btcsig: BitaxeRMT_Mods"
-
-exec /usr/local/bin/ckpool -c /config/ckpool.conf -C /config/ckpool.args
